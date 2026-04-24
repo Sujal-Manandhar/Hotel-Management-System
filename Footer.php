961f764693f3b569c5ebd870ff82426360017875
Sujal Manandhar <sujalmanandhar11@gmail.com>
2026-04-25 00:49:04 +0545

    Fix footer visibility: moved motion script to Footer.php for universal support

--- Footer.php
+++ Footer.php
@@ -71,3 +71,19 @@ if(isset($_POST['send']))
   }
 }
 ?>
+
+<script>
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    }
+  }
+}
+window.addEventListener("scroll", reveal);
+reveal(); // Initial check
+</script>
--- index.php
+++ index.php
@@ -204,22 +204,6 @@ include 'connection.php';
 </div>
 <!-- Customer Reviews Section End -->
 
-<script>
-function reveal() {
-  var reveals = document.querySelectorAll(".reveal");
-  for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
-    if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
-    }
-  }
-}
-window.addEventListener("scroll", reveal);
-reveal(); // Initial check
-</script>
-
 <?php
   include('Footer.php')
 ?>
